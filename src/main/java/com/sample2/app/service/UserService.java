d96235e7cd297bfda5f1b1d966da39b91118cf95
prashant chauhan <prashant.mikky@gmail.com>
2020-01-14 09:21:59 +0530

    lecture 15 - Pagination and Get Users API call

--- src/main/java/com/sample2/app/service/UserService.java
+++ src/main/java/com/sample2/app/service/UserService.java
@@ -1,5 +1,7 @@
 package com.sample2.app.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.sample2.app.shared.UserDto;
@@ -10,4 +12,5 @@ public interface UserService extends UserDetailsService{
 	UserDto getUserByUserId(String userId);
 	UserDto updateUser(String userId, UserDto userDto);
 	void deleteUser(String userId);
+	List<UserDto> getUsers(int page, int limit);
 }
--- src/main/java/com/sample2/app/ui/controller/UserController.java
+++ src/main/java/com/sample2/app/ui/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.sample2.app.ui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sample2.app.exceptions.UserServiceException;
@@ -82,6 +86,26 @@ public class UserController {
 		returnValue.setOperationName(RequestOpertaionName.DELETE.name());
 		userService.deleteUser(id);
 		returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+		
+		return returnValue;
+	}
+	
+	@GetMapping(produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public List<UserRest> getUsers(
+			@RequestParam(value="page", defaultValue="0") int page, 
+			@RequestParam(value="limit", defaultValue="3") int limit )
+	{		
+		List<UserRest> returnValue = new ArrayList<UserRest>();
+		List<UserDto> users = userService.getUsers(page, limit);
+		
+		for(UserDto userDto : users)
+		{
+			UserRest userModel = new UserRest();
+			BeanUtils.copyProperties(userDto, userModel);
+
+			returnValue.add(userModel);
+			
+		}
 		
 		return returnValue;
 	}
